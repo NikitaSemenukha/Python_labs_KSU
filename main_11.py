3bc2ebb31fa2daea2d3e0644fdedbf6e847a5e98
NikitaSemenukha <101047902+NikitaSemenukha@users.noreply.github.com>
2022-04-14 13:38:16 +0300

    Add files via upload

---
+++ main_11.py
@@ -0,0 +1,17 @@
+"""
+11. Дано список. Вивести спочатку всі негативні елементи, а потім всі інші.
+"""
+
+
+def main(def_list):
+    return list(filter(lambda x: x < 0, def_list)) + \
+           list(filter(lambda x: x >= 0, def_list))
+
+
+print('Enter the list: ')
+lst_ = []
+for i in range(int(input())):
+    lst_.append(int(input()))
+if __name__ == '__main__':
+    result = main(lst_)
+    print(result)
---
+++ main_16.py
@@ -0,0 +1,18 @@
+"""
+16. Дано цілочисельний список, що не містить однакових чисел. Перевірити, чи утворюють його
+елементи арифметичну прогресію (A, A + D, A + 2 • D, A + 3 • D, ....). Якщо утворюють, то вивести
+різницю прогресії, якщо ні - вивести 0.
+"""
+from operator import sub
+
+
+def main(r_):
+    r = set(map(sub, arr[1:], arr))
+    return r.pop() if len(r) == 1 else 0
+
+
+arr = list(map(int, input("Введите cписок: ").split()))
+if __name__ == '__main__':
+    result = main(arr)
+    print(result)
+input()
